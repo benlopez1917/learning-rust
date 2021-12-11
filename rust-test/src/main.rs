3e51116617326d7f7225f88a2a0fa0b82274117d
benlopez1917 <benlopez1917@tamu.edu>
2021-12-11 12:13:49 -0600

    changes to prime finder, remove redundant code

--- rust-test/src/main.rs
+++ rust-test/src/main.rs
@@ -69,12 +69,12 @@ fn prime_checker() {
         println!("{} is a prime!", n);
         return
     }
-    if n <= 1 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0 {
+    if n <= 1 {
         println!("{} is not prime.", n);
         return
     }
     
-    for i in 8 ..(n/2 + 1) {
+    for i in 2..(n/2 + 1) {
         if n % i == 0 {
             println!("{} is not prime.", n);
             return
